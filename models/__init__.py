fa710e0c988b38b16df6548a57f1e7d81faa7e45
Alex Milonakis <107515272+AlexMiloCS@users.noreply.github.com>
2026-06-10 09:03:14 +0300

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .logistic_classifier import LogisticClassifier
-from .naive_bayes import NaiveBayesClassifier
-from .svm_classifier import SVMClassifier
-from .dual_trainer import DualModelTrainer
